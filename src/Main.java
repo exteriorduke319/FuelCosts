444564ad9367883f46de4532a4492c297dae2cd6
exteriorduke319 <yarnevictanner@gmail.com>
2023-10-09 10:02:28 -0400

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,70 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double gals = 0;
+        double mpg = 0;
+        double gasPrice = 0;
+        double costPer100Mi;
+        double distanceLeft;
+        boolean validCheck1 = false;
+        boolean validCheck2 = false;
+        boolean validCheck3 = false;
+
+        do {
+            System.out.print("Please enter the current number of gallons of gas you currently have: ");
+            if (scan.hasNextDouble()) {
+                gals = scan.nextDouble();
+                scan.nextLine();
+                if (gals >= 0) {
+                    validCheck1 = true;
+                } else {
+                    System.out.println("You have entered an invalid number for gallons of gas.  Please enter a positive number.");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered an invalid number for gallons of gas.  Please enter a number.");
+            }
+        } while (!validCheck1);
+
+        do {
+            System.out.print("Please enter the fuel economy of your car in MPG: ");
+            if (scan.hasNextDouble()) {
+                mpg = scan.nextDouble();
+                scan.nextLine();
+                if (mpg >= 0) {
+                    validCheck2 = true;
+                } else {
+                    System.out.println("You have entered an invalid number for MPG.  Please enter a positive number.");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered an invalid number for MPG.  Please enter a number.");
+            }
+        } while (!validCheck2);
+
+        do {
+            System.out.print("Please enter the current gas price: ");
+            if (scan.hasNextDouble()) {
+                gasPrice = scan.nextDouble();
+                scan.nextLine();
+                if (gasPrice >= 0) {
+                    validCheck3 = true;
+                } else {
+                    System.out.println("You have entered an invalid number for gas price.  Please enter a positive number.");
+                }
+            } else {
+                scan.nextLine();
+                System.out.println("You have entered an invalid number for gas price.  Please enter a number.");
+            }
+        } while (!validCheck3);
+
+        costPer100Mi = mpg/gasPrice;
+        distanceLeft = mpg*gals;
+
+        System.out.printf("Cost per 100 miles in $: %12.2f", costPer100Mi);
+        System.out.printf("\nDistance left in miles: %12.2f", distanceLeft);
+
     }
 }
